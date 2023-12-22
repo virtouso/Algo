49fde7e0084aec42ebe4a97344471d606976eda2
virtouso <s_moeen_kamali@yahoo.com>
2023-12-23 02:32:56 +0330

    added  2 algorithms

---
+++ Algorithms/Algorithms/Problems/NumberOf1Bits.cs
@@ -0,0 +1,34 @@
+﻿namespace Algorithms.Problems
+{
+    // count number of 1s in a number
+    //https://leetcode.com/problems/number-of-1-bits/
+    public class NumberOf1Bits
+    {
+        
+        // shift one time to write and count. shift to right is like /2
+        public int HammingWeight(uint n) {
+            int count = 0;
+            while (n != 0)
+            {
+                count += (int)n & 1;
+                n >>= 1;
+            }
+            return count;
+        }
+        
+        
+        
+        //Brian Kernighan's Algorithm. its faster. less  loop
+        public  int CountSetBitsKernighan(int n)
+        {
+            int count = 0;
+            while (n != 0)
+            {
+                n &= (n - 1);
+                count++;
+            }
+            return count;
+        }
+        
+    }
+}
---
+++ Algorithms/Algorithms/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Algorithms
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
