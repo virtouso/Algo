6de6fd316ec07ea45c7afaf1738cdde91ff72f78
virtouso <s_moeen_kamali@yahoo.com>
2023-12-28 17:35:29 +0330

    count palindromic substrings

---
+++ Algorithms/Algorithms/Problems/PalindromicSubstrings.cs
@@ -0,0 +1,39 @@
+﻿namespace Algorithms.Problems
+{
+    public class PalindromicSubstrings
+    {
+        
+        // even and odd middle indexes and check they are equal.  
+        
+        public int CountSubstrings(string s)
+        {
+
+            int res = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                
+                // for odds length
+                var l = i;
+                var r = i;
+                while (l>=0 && r<s.Length && s[l]== s[r])
+                {
+                    res += 1;
+                    l--;
+                    r++;
+                }
+
+                l = i;
+                r = i + 1;
+
+                while (l>=0 && r< s.Length && s[r]==s[l])
+                {
+                    res += 1;
+                    l--;
+                    r++;
+                }
+
+            }
+            return res;
+        }
+    }
+}
