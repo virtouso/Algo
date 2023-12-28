83517f9e62411ded7615516ad146983b6e983b2f
virtouso <s_moeen_kamali@yahoo.com>
2023-12-28 15:41:30 +0330

    check palindrome

---
+++ Algorithms/Algorithms/Problems/ValidPalindrome.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Algorithms.Problems
+{
+    public class ValidPalindrome
+    {
+        // we acan also use deque or two sided queue
+        // or we can compare it with reverse: s.Reverse
+        public bool IsPalindrome(string s) {
+            s = Regex.Replace(s, @"[^a-zA-Z0-9]", "").Replace(" ","").ToLower();
+            
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left < right)
+            {
+                if (s[left] != s[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+        
+    }
+}
--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace Algorithms
+﻿using System;
+using Algorithms.Problems;
+
+namespace Algorithms
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+
+            ValidPalindrome p = new ValidPalindrome();
+            
+            Console.WriteLine(p.IsPalindrome("A man, a plan, a canal: Panama"));
+
         }
     }
 }
