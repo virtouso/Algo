daf2f5b888e9a1b39fc2f8f5b8309ec439290568
virtouso <s_moeen_kamali@yahoo.com>
2023-12-28 16:41:12 +0330

    group anagrams

---
+++ Algorithms/Algorithms/Problems/GroupAnagramsProblem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Problems
+{
+    public class GroupAnagramsProblem
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs) {
+            Dictionary<string, IList<string>> anagramGroups = new Dictionary<string, IList<string>>();
+
+            foreach (var word in strs)
+            {
+                char[] charArray = word.ToCharArray();
+                Array.Sort(charArray);
+                string key = new string(charArray);
+                
+                if (anagramGroups.ContainsKey(key))
+                {
+                    anagramGroups[key].Add(word);
+                }
+                else
+                {
+                    anagramGroups[key] = new List<string> { word };
+                }
+            }
+            
+            return anagramGroups.Values.ToList();
+        }
+    }
+}
