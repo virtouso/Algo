0fb34b2e5e14d605da61be70217507c64ef821a8
virtouso <s_moeen_kamali@yahoo.com>
2024-01-20 18:38:10 +0330

    added valid parantheses

---
+++ Algorithms/Algorithms/Problems/ValidParentheses.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Problems
+{
+    public class ValidParentheses
+    {
+        public bool IsValid(string s) {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char ch in s) {
+                if (ch == '(' || ch == '{' || ch == '[') {
+                    stack.Push(ch);
+                } else {
+                    if (stack.Count == 0) {
+                        return false; 
+                    }
+
+                    char top = stack.Pop();
+
+              
+                    if ((ch == ')' && top != '(') || 
+                        (ch == '}' && top != '{') || 
+                        (ch == ']' && top != '[')) {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.Count == 0; 
+        }
+    }
+}
