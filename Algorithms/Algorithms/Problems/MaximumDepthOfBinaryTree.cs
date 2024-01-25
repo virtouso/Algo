1c30fa8b59ca4f583ab3316eeb1cf653a3cb01f2
virtouso <s_moeen_kamali@yahoo.com>
2024-01-25 14:05:20 +0330

    maximum depth of binary tree

---
+++ Algorithms/Algorithms/Problems/MaximumDepthOfBinaryTree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Algorithms.Problems
+{
+    public class MaximumDepthOfBinaryTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int value)
+            {
+                val = value;
+                left = null;
+                right = null;
+            }
+        }
+
+        public int MaxDepth(TreeNode node)
+        {
+            if (node == null)
+                return 0;
+
+            int leftDepth = MaxDepth(node.left);
+            int rightDepth = MaxDepth(node.right);
+
+            return Math.Max(leftDepth, rightDepth) + 1;
+        }
+    }
+}
