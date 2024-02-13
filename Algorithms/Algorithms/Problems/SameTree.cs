79bac22a6707dd8f1fc3b008d29e42bdf6f3f137
virtouso <s_moeen_kamali@yahoo.com>
2024-02-13 10:57:42 +0330

    added same tree

---
+++ Algorithms/Algorithms/Problems/SameTree.cs
@@ -0,0 +1,31 @@
+﻿namespace Algorithms.Problems
+{
+    public class SameTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+                return true;
+            if (p == null || q == null)
+                return false;
+            if (p.val != q.val)
+                return false;
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+    }
+}
