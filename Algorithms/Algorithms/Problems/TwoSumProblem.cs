df6a6873c4246b1be6d55dd6434cb4117694d3f0
virtouso <s_moeen_kamali@yahoo.com>
2024-02-13 18:06:46 +0330

    two sum

---
+++ Algorithms/Algorithms/Problems/TwoSumProblem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Problems
+{
+    public class TwoSumProblem
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> refs = new Dictionary<int, int>();
+            var result = new int[2];
+            refs.Add(target - nums[0], 0);
+            for (int i = 1; i < nums.Length; i++)
+            {
+                var goal = target - nums[i];
+                if (refs.ContainsKey(target - goal))
+                {
+                    result[1] = i;
+                    result[0] = refs[target - goal];
+                    break;
+                }
+
+                else
+                {
+                    if (!refs.ContainsKey(goal))
+                        refs.Add(goal, i);
+                }
+            }
+
+            return result;
+        }
+    }
+}
