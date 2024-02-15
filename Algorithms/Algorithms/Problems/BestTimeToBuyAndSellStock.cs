a4bc6f2aec5707cb306f5ecc9ffd7196f5c76494
virtouso <s_moeen_kamali@yahoo.com>
2024-02-15 21:54:32 +0330

    best time to sell

---
+++ Algorithms/Algorithms/Problems/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Algorithms.Problems
+{
+    public class BestTimeToBuyAndSellStock
+    {
+        public int MaxProfit(int[] prices) {
+            int l = 0;
+            int r = 1;
+            int best = 0;
+
+            while (r < prices.Length)
+            {
+                if (prices[r] > prices[l])
+                {
+                    var prof = prices[r] - prices[l];
+                    best = Math.Max(prof, best);
+                }
+                else
+                {
+                    l = r;
+                }
+
+                r++;
+            }
+
+            return best;
+        }
+    }
+}
