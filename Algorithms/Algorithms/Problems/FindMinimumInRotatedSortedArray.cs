88fc595e5029f66e34018b99be57c0a32b2aa4de
virtouso <s_moeen_kamali@yahoo.com>
2024-02-29 14:56:32 +0330

    minimum in rotated array.

---
+++ Algorithms/Algorithms/Problems/FindMinimumInRotatedSortedArray.cs
@@ -0,0 +1,18 @@
+﻿namespace Algorithms.Problems
+{
+    public class FindMinimumInRotatedSortedArray
+    {
+        public int FindMin(int[] nums)
+        {
+            var result = nums[0];
+
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] < nums[i - 1]) return nums[i];
+            }
+
+            return result;
+        }
+    }
+}
--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -7,8 +7,11 @@ namespace Algorithms
     {
         public static void Main(string[] args)
         {
-        
 
+            FindMinimumInRotatedSortedArray ar = new FindMinimumInRotatedSortedArray();
+            Console.WriteLine(ar.FindMin(new []{3,4,5,1,2}));
+            Console.WriteLine(ar.FindMin(new []{4,5,6,7,0,1,2}));
+            Console.WriteLine(ar.FindMin(new []{11,13,15,17}));
         }
     }
 }
