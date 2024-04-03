f27f2bbc6d786017667980ea5fdd118ef7c653b9
virtouso <s_moeen_kamali@yahoo.com>
2024-04-03 16:20:53 +0330

    reverse bits

---
+++ Algorithms/Algorithms/Problems/ReverseBitsProblem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace Algorithms.Problems
+{
+    public class ReverseBitsProblem
+    {
+        public uint ReverseBits(uint n) {
+            uint ans = 0;
+            int power = 31;
+            while(n!=0){
+                ans += (n&1)<<power;
+                n = n>>1;
+                power--;
+            }
+            return ans;
+            
+        }
+    }
+}
