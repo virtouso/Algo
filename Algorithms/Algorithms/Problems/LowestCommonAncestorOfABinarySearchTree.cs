fe53c6157127abf177dbfa0ff6a7e124f931240f
virtouso <s_moeen_kamali@yahoo.com>
2024-04-06 01:45:46 +0330

    Lowest Common Ancestor of a Binary Search Tree

---
+++ Algorithms/Algorithms/Problems/LowestCommonAncestorOfABinarySearchTree.cs
@@ -0,0 +1,31 @@
+﻿namespace Algorithms.Problems
+{
+    public class LowestCommonAncestorOfABinarySearchTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int x)
+            {
+                val = x;
+            }
+        }
+
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root == null)
+                return null;
+
+            if (p.val < root.val && q.val < root.val)
+                return LowestCommonAncestor(root.left, p, q);
+            else if (p.val > root.val && q.val > root.val)
+                return LowestCommonAncestor(root.right, p, q);
+            else
+                return root;
+            
+        }
+    }
+}
