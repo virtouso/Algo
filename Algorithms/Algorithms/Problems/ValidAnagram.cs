18b0672c5c8c555e4d9ab59291c9de44a15d357f
virtouso <s_moeen_kamali@yahoo.com>
2024-04-08 01:11:43 +0330

    word break

--- Algorithms/Algorithms/Problems/ValidAnagram.cs
+++ Algorithms/Algorithms/Problems/ValidAnagram.cs
@@ -5,17 +5,15 @@ namespace Algorithms.Problems
 {
     public class ValidAnagram
     {
-        
         // check is the second one is reordered version of another. 
         // use dictionary or hashmap. but has memory problem.
         // use sorting. time complexity is more but no more memory
-        
+
         public bool IsAnagram(string s, string t)
         {
-        s=    String.Concat(s.OrderBy(c => c));
-        t=    String.Concat(t.OrderBy(c => c));
-        return s == t;
-
+            s = String.Concat(s.OrderBy(c => c));
+            t = String.Concat(t.OrderBy(c => c));
+            return s == t;
         }
     }
 }
---
+++ Algorithms/Algorithms/Problems/WordBreakProblem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Problems
+{
+    public class WordBreakProblem
+    {
+        // Example 1:
+        //
+        // Input: s = "leetcode", wordDict = ["leet","code"]
+        // Output: true
+        // Explanation: Return true because "leetcode" can be segmented as "leet code".
+        // Example 2:
+        //
+        // Input: s = "applepenapple", wordDict = ["apple","pen"]
+        // Output: true
+        // Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+        // Note that you are allowed to reuse a dictionary word.
+        //     Example 3:
+        //
+        // Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+        // Output: false
+
+
+        //method 1: DP
+        // method 2: decision tree
+
+        public bool WordBreak(string s, IList<string> wordDict)
+        {
+            HashSet<string> wordSet = new HashSet<string>(wordDict);
+            int n = s.Length;
+            bool[] dp = new bool[n + 1];
+            dp[0] = true; // Empty string is always a valid word break
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (dp[j] && wordSet.Contains(s.Substring(j, i - j)))
+                    {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
