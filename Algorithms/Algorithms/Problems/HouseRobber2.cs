d61eb7f97b1f2eb0966d0d2afe1e2fa22b4b476d
virtouso <s_moeen_kamali@yahoo.com>
2024-04-10 14:57:47 +0330

    house robber 2

---
+++ Algorithms/Algorithms/Problems/HouseRobber2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Algorithms.Problems
+{
+    public class HouseRobber2
+    {
+        // house robber but first house and last house in list are connected
+        
+        public int Rob(int[] nums) {
+            if (nums == null || nums.Length == 0) return 0;
+            if (nums.Length == 1) return nums[0];
+        
+            // Maximum amount if we rob the first house
+            int max1 = RobHelper(nums, 0, nums.Length - 2);
+            // Maximum amount if we don't rob the first house
+            int max2 = RobHelper(nums, 1, nums.Length - 1);
+        
+            return Math.Max(max1, max2);
+        }
+    
+        private int RobHelper(int[] nums, int start, int end) {
+            int prevMax = 0;
+            int currMax = 0;
+        
+            for (int i = start; i <= end; i++) {
+                int temp = currMax;
+                currMax = Math.Max(prevMax + nums[i], currMax);
+                prevMax = temp;
+            }
+        
+            return currMax;
+        }
+    }
+}
