31768e5658589fb6f97db7c8846a0f2f0fc786b9
virtouso <s_moeen_kamali@yahoo.com>
2024-04-20 22:10:42 +0330

    clone graph

---
+++ Algorithms/Algorithms/Problems/CloneGraphProblem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Problems
+{
+    public class CloneGraphProblem
+    {
+        public class Node {
+            public int val;
+            public List<Node> neighbors;
+
+            public Node() {
+                val = 0;
+                neighbors = new List<Node>();
+            }
+
+            public Node(int _val) {
+                val = _val;
+                neighbors = new List<Node>();
+            }
+
+            public Node(int _val, List<Node> _neighbors) {
+                val = _val;
+                neighbors = _neighbors;
+            }
+        }
+        
+           private Dictionary<Node, Node> visited = new Dictionary<Node, Node>();
+        
+            public Node CloneGraph(Node node) {
+                if (node == null) return null;
+        
+                if (visited.ContainsKey(node)) {
+                    return visited[node];
+                }
+        
+                Node cloneNode = new Node(node.val);
+                visited.Add(node, cloneNode);
+        
+                foreach (Node neighbor in node.neighbors) {
+                    cloneNode.neighbors.Add(CloneGraph(neighbor));
+                }
+        
+                return cloneNode;
+            }
+    }
+}
---
+++ Algorithms/Algorithms/Problems/JumpGame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Algorithms.Problems
+{
+    public class JumpGame
+    {
+        
+        // You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+        //
+        //     Return true if you can reach the last index, or false otherwise.
+        
+        public bool CanJump(int[] nums)
+        {
+            int maxReach = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i > maxReach)
+                {
+                    return false;
+                }
+
+                maxReach = Math.Max(maxReach, i + nums[i]);
+                if (maxReach >= nums.Length - 1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
