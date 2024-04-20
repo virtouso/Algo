4a6d72dbb152df002fa1c67e422d88cbd5334943
virtouso <s_moeen_kamali@yahoo.com>
2024-04-20 14:46:49 +0330

    added count intervals.

--- Algorithms/Algorithms/Problems/CoutTimeIntervalsInPeriod.cs
+++ Algorithms/Algorithms/Problems/CoutTimeIntervalsInPeriod.cs
@@ -4,8 +4,34 @@
     {
         public int CountIntervals(string start, string end)
         {
-            var startNumber = start.Split(':');
-            var endNumber = end.Split(':');
+            var sn = start.Split(':');
+            var en = end.Split(':');
+            var sh = start[0];
+            var sm = start[1];
+            var eh = end[0];
+            var em = end[1];
+
+            var s = sh * 60 + sm;
+            var e = eh * 60 + em;
+
+            if (e < s)
+                e = (12 * 60) - s + e;
+
+            int counter = 0;
+            int state = s;
+            while (state<e)
+            {
+                if (state + 15 < e) counter++;
+                state += 15;
+            }
+
+            return counter;
+        }
+
+
+        public void Run()
+        {
+            
         }
     }
 }
---
+++ Algorithms/Algorithms/Problems/UniquePathsProblem.cs
@@ -0,0 +1,36 @@
+﻿namespace Algorithms.Problems
+{
+    public class UniquePathsProblem
+    {
+        // There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+        //
+        //     Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+        //
+        //     The test cases are generated so that the answer will be less than or equal to 2 * 109.
+        
+        
+        
+        public int UniquePaths(int m, int n) {
+            // Create a 2D array to store the number of unique paths to reach each cell
+            int[,] dp = new int[m, n];
+        
+            // Initialize the number of unique paths for the first row and first column to 1
+            for (int i = 0; i < m; i++) {
+                dp[i, 0] = 1;
+            }
+            for (int j = 0; j < n; j++) {
+                dp[0, j] = 1;
+            }
+        
+            // Fill up the dp array with the number of unique paths
+            for (int i = 1; i < m; i++) {
+                for (int j = 1; j < n; j++) {
+                    dp[i, j] = dp[i - 1, j] + dp[i, j - 1];
+                }
+            }
+        
+            // The value at dp[m-1, n-1] will be the number of unique paths to reach the bottom-right cell
+            return dp[m - 1, n - 1];
+        } 
+    }
+}
