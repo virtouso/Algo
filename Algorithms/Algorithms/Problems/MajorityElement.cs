cb2717fd40fba9d2f778386adc90cdf2411677e0
virtouso <s_moeen_kamali@yahoo.com>
2024-04-29 00:39:55 +0330

    find majority ellement

---
+++ Algorithms/Algorithms/Problems/MajorityElement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Algorithms.Problems
+{
+    public class MajorityElement
+    {
+        static int FindMajorityElement(int[] nums)
+        {
+            int candidate = 0;
+            int count = 0;
+
+            foreach (int num in nums)
+            {
+                if (count == 0)
+                {
+                    candidate = num;
+                    count = 1;
+                }
+                else if (num == candidate)
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+
+            // At this point, candidate is a potential majority element, but we need to verify it.
+            count = 0;
+            foreach (int num in nums)
+            {
+                if (num == candidate)
+                {
+                    count++;
+                }
+            }
+
+            // If the count of candidate is greater than half the size of the array, then it's the majority element.
+            if (count > nums.Length / 2)
+            {
+                return candidate;
+            }
+            else
+            {
+                // If there is no majority element, you might return -1 or throw an exception depending on your requirement.
+                throw new Exception("No majority element found.");
+            }
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Problems/TowerOfHanoi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Problems
+{
+    public class TowerOfHanoi
+    {
+        public  void MoveDisks(int disks, Stack<int> source, Stack<int> auxiliary, Stack<int> destination)
+        {
+            if (disks > 0)
+            {
+                // Move top (disks - 1) disks from source to auxiliary using destination as auxiliary
+                MoveDisks(disks - 1, source, destination, auxiliary);
+
+                // Move the nth disk from source to destination
+                int disk = source.Pop();
+                destination.Push(disk);
+                Console.WriteLine($"Move disk {disk} from Source to Destination");
+
+                // Move (disks - 1) disks from auxiliary to destination using source as auxiliary
+                MoveDisks(disks - 1, auxiliary, source, destination);
+            }
+        }
+    }
+}
